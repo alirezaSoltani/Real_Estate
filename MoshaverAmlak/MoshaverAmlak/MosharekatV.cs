f6d529f54261c785c0b88ac0df78bc3c39e2ca63
alirezaSoltani <alireza.soltani1373@gmail.com>
2017-02-03 23:36:08 -0800

    font2

---
+++ MoshaverAmlak/MoshaverAmlak/MosharekatV.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshaverAmlak
+{
+    class MosharekatV
+    {
+        private int mosharekatV_id;
+        private string mosharekatV_tozih;
+        private string mosharekatV_malekncode;
+        private int mosharekatV_vilayiid;
+        private string mosharekatV_noeSanad;
+        private int mosharekatV_tedadDang;
+
+        public int MosharekatV_id
+        {
+            get
+            {
+                return mosharekatV_id;
+            }
+
+            set
+            {
+                mosharekatV_id = value;
+            }
+        }
+
+        public string MosharekatV_tozih
+        {
+            get
+            {
+                return mosharekatV_tozih;
+            }
+
+            set
+            {
+                mosharekatV_tozih = value;
+            }
+        }
+
+        public string MosharekatV_malekncode
+        {
+            get
+            {
+                return mosharekatV_malekncode;
+            }
+
+            set
+            {
+                mosharekatV_malekncode = value;
+            }
+        }
+
+        public int MosharekatV_vilayiid
+        {
+            get
+            {
+                return mosharekatV_vilayiid;
+            }
+
+            set
+            {
+                mosharekatV_vilayiid = value;
+            }
+        }
+
+        public string MosharekatV_noeSanad
+        {
+            get
+            {
+                return mosharekatV_noeSanad;
+            }
+
+            set
+            {
+                mosharekatV_noeSanad = value;
+            }
+        }
+
+        public int MosharekatV_tedadDang
+        {
+            get
+            {
+                return mosharekatV_tedadDang;
+            }
+
+            set
+            {
+                mosharekatV_tedadDang = value;
+            }
+        }
+    }
+}
